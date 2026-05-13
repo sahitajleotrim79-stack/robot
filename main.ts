e1e81949654fb5176cbfbb2896296b24df59a217
sahitajleotrim79-stack <sahitajleotrim79@gmail.com>
2026-05-13 22:07:59 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-CutebotPro.cruiseControl(90, 90, CutebotProSpeedUnits.Ins)
+basic.forever(function () {
+    if (true) {
+        gigglebot.turnMillisec(gigglebotWhichTurnDirection.Right, 1000)
+    }
+    if (true) {
+        gigglebot.turnMillisec(gigglebotWhichTurnDirection.Left, 1000)
+    }
+    if (true) {
+        gigglebot.lightFollow()
+    }
+})
